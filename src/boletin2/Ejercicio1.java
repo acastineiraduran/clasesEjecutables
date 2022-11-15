4af34c323cefd5f0e6ffa5b5db5fe9824c8d697c
acastineiraduran <acastineiraduran@danielcastelao.org>
2022-11-15 23:32:31 +0100

    modificacion del ejercico 1 del boletin 2

--- src/boletin2/Ejercicio1.java
+++ src/boletin2/Ejercicio1.java
@@ -3,5 +3,6 @@ package boletin2;
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.println(" Este es el ejercicio 1 del boletin 2");
+        System.out.println("Esta es mi tercera modificacion");
     }
 }
